6107a69dff4fb88df9ebc3b945f383a1b2a69fed
Sudeesh John <sudeeshjon@in.ibm.com>
2019-08-31 11:03:59 +0530

    Fixing issues with dynamic compute template creation

--- vars/createTemplate.groovy
+++ vars/createTemplate.groovy
@@ -1,11 +1,5 @@
 def call(String auth_url, String vcpus, String memory, String punits, String templatetag){
     // this creates a compute template in PowerVC server based on the choice
-    environment {
-      OS_USERNAME=credentials('GITHUB_USER')
-      OS_PASSWORD=credentials('TF_VAR_password')
-      OS_IDENTITY_API_VERSION="${env.OS_IDENTITY_API_VERSION}"
-      OS_TENANT_NAME="${env.OS_TENANT_NAME}"
-    }
     def status = sh(returnStatus: true, returnStdout: false, script: "openstack  --os-auth-url \"${auth_url}\" --insecure flavor create --id ${templatetag}  --ram ${memory} --vcpus ${vcpus} ${templatetag} --property powervm:proc_units=${punits}")
     return status
 }
--- vars/getArtifactoryAndClean.groovy
+++ vars/getArtifactoryAndClean.groovy
@@ -3,8 +3,8 @@ def call(String auth_url)
     // Updating tempate file with env variables
     sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${auth_url}\" --insecure flavor delete ${env.MASTER_TEMPLATE}")
     sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${auth_url}\" --insecure flavor delete ${env.WORKER_TEMPLATE}")
-    sh (returnStdout: false, script: "/bin/bash ${WORKSPACE}/hack/capture_artifacts.sh")
+    sh (returnStdout: false, script: "/bin/bash ${WORKSPACE}/hack/capture_artifacts.sh || true")
     KEYPAIR=sh(returnStdout: true, script: "cd ${WORKSPACE}/canary-deployments && make terraform:output TERRAFORM_DIR=.deploy-power-powervc TERRAFORM_OUTPUT_VAR=keypair-name || true") 
-    sh (returnStdout: false, script: "cd ${WORKSPACE}/canary-deployments && make $TARGET:clean")
+    sh (returnStdout: false, script: "cd ${WORKSPACE}/canary-deployments && make $TARGET:clean || true")
     sh (returnStdout: false, script: "openstack --os-auth-url \"${auth_url}\" --insecure keypair delete \"${KEYPAIR}\"")
 }
