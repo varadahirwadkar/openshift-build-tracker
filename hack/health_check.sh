49fed54cbe2fe3232b512d21119016c844542793
Sudeesh John <sudeeshjon@in.ibm.com>
2019-09-24 13:52:10 +0530

    correcting the inception image

--- hack/health_check.sh
+++ hack/health_check.sh
@@ -2,9 +2,6 @@
 # -health check
 if [ ${OCP_ENV} == true ];then install_dir="/root/" ;else install_dir="/opt/ibm";fi
 echo -e "***** Getting cluster Info ICP Cluster *****"
-[ -d ${install_dir}/cluster ] && cd ${install_dir}/cluster
-docker run --net=host -t -e LICENSE=accept -v "$(pwd)":/installer/cluster ${inception} healthcheck || exit 0
-echo -e "***** Getting cluster Info from OCP-ICP clusrer *****"
-[ -d /root/cluster ] && cd /root/cluster
 inception=$(docker images | grep inception | awk '{print $3}')
+[ -d ${install_dir}/cluster ] && cd ${install_dir}/cluster
 docker run --net=host -t -e LICENSE=accept -v "$(pwd)":/installer/cluster ${inception} healthcheck || exit 0
