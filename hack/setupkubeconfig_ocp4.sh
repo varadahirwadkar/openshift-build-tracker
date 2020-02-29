bfe1e1ab347c10fdb72161a2cb47af27b1681fb2
Sudeesh John <sudeeshjon@in.ibm.com>
2020-02-29 22:48:07 +0530

    OpenShift4.x support

---
+++ hack/setupkubeconfig_ocp4.sh
@@ -0,0 +1,24 @@
+    #!/bin/bash -x
+    echo "Capturing the System Information"
+    if [ -d ${WORKSPACE}/deploy ];then
+        cd ${WORKSPACE}/deploy
+    else
+        exit 1 
+    fi
+    # Capturing Teraform template
+    BASTION_IP=$(make terraform:output TERRAFORM_DIR=.${TARGET} TERRAFORM_OUTPUT_VAR=bastion_ip || true)
+    [ $? -ne 0 ] && exit 1;
+    if [ ! -z "${BASTION_IP}" ]; then
+        ssh -q -i id_rsa -o StrictHostKeyChecking=no root@${BASTION_IP} exit
+        rc=$?
+        if [ $? -eq 0 ] ; then
+            rm -rf ~/.kube
+            mkdir ~/.kube
+            scp -i id_rsa -o StrictHostKeyChecking=no  root@${MASTER_NODE}:/root/openstack-upi/auth/kubeconfig ~/.kube/config
+            make terraform:output TERRAFORM_DIR=.${TARGET} TERRAFORM_OUTPUT_VAR=etc_hosts_entries >> /etc/hosts
+        else
+            echo 'Unable to access the cluster. You may delete the VMs manually'
+        fi
+    else
+        echo 'Unable to access the cluster. You may delete the VMs manually'
+    fi
--- vars/getTheImage.groovy
+++ vars/getTheImage.groovy
@@ -1,11 +1,27 @@
 def call(String auth_url, String distro, String filter){
     // this get the image from powervc based on the distro chose.
     switch(distro) {
+        case "rhcos-43":
+            IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i rhcos-43 | tail -n 1|tr '\n' ' ' ").trim()
+            env.DISTRO="rhel"
+            break
+        case "rhcos-44":
+            IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i rhcos-44 | tail -n 1|tr '\n' ' ' ").trim()
+            env.DISTRO="rhel"
+            break
+        case "RHEL8.0":
+            IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i rhel8.0| tail -n 1|tr '\n' ' ' ").trim()
+            env.DISTRO="rhel"
+            break
+        case "RHEL8.1":
+            IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i rhel8.4| tail -n 1|tr '\n' ' ' ").trim()
+            env.DISTRO="rhel"
+            break
         case "RHEL7.7":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i \"${filter}\" | grep -i rhel7.7| tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
             break
-	case "RHEL7.6":
+	    case "RHEL7.6":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i \"${filter}\" | grep -i rhel7.6| grep -i -v  rhel7.6-alt|tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
             break
