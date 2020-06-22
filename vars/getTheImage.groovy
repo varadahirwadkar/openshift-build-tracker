b46ed6c29ddf6bda858eb4dab0e1e9134a87af4e
Sudeesh John <sudeeshjon@in.ibm.com>
2020-06-22 15:43:11 +0530

    Fixing rhel release issue

--- vars/getTheImage.groovy
+++ vars/getTheImage.groovy
@@ -20,26 +20,32 @@ def call(String auth_url, String distro, String filter){
         case "RHEL8.0":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i rhel8.0| tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
+            env.REDHAT_RELEASE="8.0"
             break
         case "RHEL8.1":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i rhel8.1| tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
+            env.REDHAT_RELEASE="8.1"
             break
         case "RHEL7.8":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i \"${filter}\" | grep -i rhel7.8| tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
+            env.REDHAT_RELEASE="7.8"
             break
         case "RHEL7.7":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i \"${filter}\" | grep -i rhel7.7| tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
+            env.REDHAT_RELEASE="7.7"
             break
         case "RHEL7.6":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i \"${filter}\" | grep -i rhel7.6| grep -i -v  rhel7.6-alt|tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
+            env.REDHAT_RELEASE="7.6"
             break
         case "RHEL7.5":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list  --format value -c Name | grep -i \"${filter}\" | grep -i rhel7.5| grep -i -v rhel7.5-alt|tail -n 1|tr '\n' ' ' ").trim()
             env.DISTRO="rhel"
+            env.REDHAT_RELEASE="7.5"
             break
         case "UBUNTU18.04":
             IMAGE=sh(returnStdout: true, script: "openstack --os-auth-url \"${auth_url}\" --insecure image list --format value -c Name | grep -i \"${filter}\" | grep -i u18.04| tail -n 1|tr '\n' ' ' ").trim()
