e35dbf7bac09cc3cb6c0fddcf6d06368dda62a04
Sudeesh John <sudeeshjon@in.ibm.com>
2020-10-11 11:50:54 +0530

    calling cleanup again

--- vars/getArtifactsAndCleanOcp4.groovy
+++ vars/getArtifactsAndCleanOcp4.groovy
@@ -3,6 +3,8 @@ def call(String auth_url)
     // Updating tempate file with env variables
     sh (returnStdout: false, script: "/bin/bash ${WORKSPACE}/hack/capture-artifacts-ocp4.sh || true")
     sh (returnStdout: false, script: "cd ${WORKSPACE}/deploy && make $TARGET:clean || true")
+    sh (returnStdout: false, script: "cd ${WORKSPACE}/deploy && make $TARGET:clean || true")
+    sh (returnStdout: false, script: "cd ${WORKSPACE}/deploy && make $TARGET:clean || true")
     if ( "${POWERVS}" == "false" ) {
         sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${auth_url}\" --insecure flavor delete ${env.MASTER_TEMPLATE}")
         sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${auth_url}\" --insecure flavor delete ${env.WORKER_TEMPLATE}")
