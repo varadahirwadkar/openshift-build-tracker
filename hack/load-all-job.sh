0ba06220b31070ffac644fc07f4d01d627b5eeae
sudeeshjohn <sudeeshjon@in.ibm.com>
2021-04-06 00:22:34 +0530

    jjb config files

--- hack/load-all-job.sh
+++ hack/load-all-job.sh
@@ -6,5 +6,5 @@ set -o pipefail
 
 INFRA_ROOT="$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)"
 
-${INFRA_ROOT}/hack/load-jenkins-jjbs.sh
+#${INFRA_ROOT}/hack/load-jenkins-jjbs.sh
 ${INFRA_ROOT}/hack/load-jenkins-pipelines.sh
