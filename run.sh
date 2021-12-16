a84277d879ba22db5398c919b192bc2b0a84a84f
Varad Ahirwadkar <varad.ahirwadkar@ibm.com>
2021-12-16 11:19:56 +0530

    modified ryn.sh
    Signed-off-by: Varad Ahirwadkar <varad.ahirwadkar@ibm.com>

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ run_script() {
   for x in 6 7 8 9 10
   do
   VERSION=`curl -X GET "https://quay.io/api/v1/repository/openshift-release-dev/ocp-release/tag/?onlyActiveTags=true" | jq '.tags | sort_by(.start_ts) | .[].name'  | grep 4.$x | grep "ppc64le"  | grep -v multi | tail -1 |  tr -d '"'`
-  if [ "$(cat versions/4.$x.txt)" != $VERSION ];
+  if [[ "$(cat versions/4.$x.txt)" != "$VERSION" && "$VERSION" != "" ]];
   then
   echo $VERSION > versions/4.$x.txt
   fi
