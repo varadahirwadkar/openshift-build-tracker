45082c941802c8e92912de5eb735ee308d2adaa5
Varad Ahirwadkar <varad.ahirwadkar@ibm.com>
2021-12-16 10:26:34 +0530

    modified run job
    Signed-off-by: Varad Ahirwadkar <varad.ahirwadkar@ibm.com>

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 run_script() {
-  for x in 6 7 8 9
+  if [ ! -d "versions" ];
+    then
+        mkdir  "versions"
+  fi
+  for x in 6 7 8 9 10
   do
   VERSION=`curl -X GET "https://quay.io/api/v1/repository/openshift-release-dev/ocp-release/tag/?onlyActiveTags=true" | jq '.tags | sort_by(.start_ts) | .[].name'  | grep 4.$x | grep "ppc64le"  | grep -v multi | tail -1 |  tr -d '"'`
-  if [ "$(cat versions/4.$x.txt)" != $VERSION ];
+  if [ "$(cat versions/4.$x.txt)" != "$VERSION" ];
   then
   echo $VERSION > versions/4.$x.txt
   fi
